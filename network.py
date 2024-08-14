7253fe1c844607e51e5bb4fe93f11d7c25172b0a
Álvaro  Camacho Hernandez <32852760+acamher@users.noreply.github.com>
2024-08-14 23:10:02 +0200

    network data storedi n json file

--- network.py
+++ network.py
@@ -1,21 +1,23 @@
+import json
 from classes import network as net
 from classes import node as nd
 from classes import leg as lg
 
 # Creating Network
+f = open('network/A.json')
+data = json.load(f)
 
-network = net.Network("Network_A")
+network = net.Network(data['name'])
+node_list = {}
+leg_list = {}
 
-node_1 = nd.Node(network, "A", -1, 1)
-node_2 = nd.Node(network, "B", -1, -1)
-node_3 = nd.Node(network, "C", 1, -1)
-node_4 = nd.Node(network, 4, 2,2)
+for node in data['nodes']:
+    node_list[node['id']] = nd.Node(network, node['id'], node['x'], node['y'])
 
-leg_1 = lg.Leg(node_1, node_2)
-leg_2 = lg.Leg(node_2, node_3)
-leg_3 = lg.Leg(node_1, node_3)
-leg_4 = lg.Leg(node_1, node_4)
-leg_5 = lg.Leg(node_4, node_2)
+n = 0
+for leg in data['legs']:
+    leg_list[str(n)] = lg.Leg(node_list[str(leg['start'])], node_list[str(leg['finish'])])
+    n += 1
 
 # Actions
 network.printGraph()
---
+++ notes.py
@@ -0,0 +1,14 @@
+#OLD WAY TO CREATE NETWORK
+
+network = net.Network("Network_A")
+
+node_1 = nd.Node(network, "A", -1, 1)
+node_2 = nd.Node(network, "B", -1, -1)
+node_3 = nd.Node(network, "C", 1, -1)
+node_4 = nd.Node(network, 4, 2,2)
+
+leg_1 = lg.Leg(node_1, node_2)
+leg_2 = lg.Leg(node_2, node_3)
+leg_3 = lg.Leg(node_1, node_3)
+leg_4 = lg.Leg(node_1, node_4)
+leg_5 = lg.Leg(node_4, node_2)
